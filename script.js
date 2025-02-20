9a71968732099f5cd1402476480b0153fdf5dd93
Sarmitha769 <sarmithasaji.01@gmail.com>
2025-02-20 16:01:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitData(){
+    var name=document.getElementById('name').Value
+    var dob=document.grtElementById('dob').Value
+    console.log(name+""+dob)
+    var output=document.getElementById('output');
+    output.innerText
+}
